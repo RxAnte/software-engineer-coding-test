2383be6a5df320f42fe420f17b2e8c182f633212
TJ Draper <tdraper@rxante.com>
2023-10-16 16:46:26 -0500

    Start setting up the next web app

---
+++ web/next.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+    publicRuntimeConfig: {
+    },
+    serverRuntimeConfig: {
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    poweredByHeader: false,
+    reactStrictMode: true,
+};
